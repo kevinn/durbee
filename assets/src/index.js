d4d9f9326a03df8be6a5725787a57565ae98cbc8
kevinn <knavia@gmail.com>
2019-03-21 11:42:55 +0800

    refactoring

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,34 +1,43 @@
 const vm = new Vue({
   el: "#app",
-  data() {
-    return {
-      timer: null,
-      timerPaused: true,
-      timerSelected: '',
-      timerElapsed: null,
-      timerlength: [
-        { 'length': '3 minutes', 'ms': 180000 },
-        { 'length': '2 minutes', 'ms': 120000 },
-        { 'length': '1 minute', 'ms': 60000 },
-        { 'length': '5 seconds', 'ms': 5000 } // testing
-      ],
-      workoutList: [],
-      workoutSelectedData: [],
-      workoutopen: false,
-      currentWorkout: null,
-      currentWorkoutImg: null,
-      currentWorkoutNum: null,
-      disableNext: false,
-      disablePrev: true
-    }
+  data: {
+    workoutList: [],
+    workoutSelectedData: [],
+    isTimerSelected: '',
+    timer: null,
+    timerPaused: true,
+    timerElapsed: null,
+    timerChoices: [
+      { 'length': '3 minutes', 'ms': 180000 },
+      { 'length': '2 minutes', 'ms': 120000 },
+      { 'length': '1 minute', 'ms': 60000 },
+      { 'length': '3 seconds (test)', 'ms': 3000 } // testing
+    ],
+    currWorkout: null,
+    currWorkoutImg: null,
+    currWorkoutNum: null,
+    disableNext: false,
+    disablePrev: true,
+    modalOpen: false,
+    isAutoplay: false
   },
   methods: {
-    initList: function() {
-      const list = vm.workoutList;
+    modal: function(action) {
+      if (action == 'show') {
+        vm.modalOpen = true;
+      } else {
+        vm.modalOpen = false;
+      }
+    },
+    modalClose: function() {
+      // reset everything?
+      vm.modalOpen = false;
+      vm.workoutClear();
     },
     selectChange: function() {
       // assign to variable for countdown
-      vm.timerElapsed = vm.timerSelected;
+      vm.timerElapsed = vm.isTimerSelected;
+      console.log('time is set to: ', vm.timerElapsed);
     },
     timerStart: function() {
       vm.timerPaused = false;
@@ -43,76 +52,32 @@ const vm = new Vue({
     timerReset: function() {
       // reset back to timerSelected;
       clearInterval(vm.timer);
-      vm.timerElapsed = vm.timerSelected;
+      vm.timerElapsed = vm.isTimerSelected;
       vm.timerPaused = true;
-    },
-    timerDisplay: function() {
-      // display timeElapsed in 00:00 format
-      const vm = this;
-      let ms = vm.timerElapsed;
-
-      // https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
-      let minutes = Math.floor( ms / 60000);
-      let seconds = ((ms % 60000) / 1000).toFixed(0);
-      return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
     },
     countdown: function() {
       if (vm.timerElapsed !== 0) {
         vm.timerElapsed = vm.timerElapsed - 1000;
-        vm.timerDisplay(vm.timerElapsed);
-        vm.beep();
+        vm.beep('beep');
       } else {
-        vm.beepEnd();
-        setTimeout(()=>{
-          vm.timerReset();
-        }, 1500);
+        vm.beep('prrrt');
+        vm.timerReset();
       }
     },
-    beep: function() {
-      const audioEl = document.getElementById('fx');
+    beep: function(soundId) {
+      const audioEl = document.getElementById(soundId);
       audioEl.currentTime = 0;
       audioEl.play();
-    },
-    beepEnd: function() {
-      const beep = document.getElementById('fx');
-      beep.pause();
-
-      const audioEl = document.getElementById('fxEnd');
-      audioEl.currentTime = 0;
-      audioEl.play();
-      setTimeout(() => {
-        audioEl.pause();
-      },1060);
-    },
-    greeting: function() {
-      const today = new Date().getHours();
-      let greet;
-
-      if (today < 12) {
-        greet = 'morning';
-      } else if (today < 18) {
-        greet = 'afternoon';
-      } else {
-        greet = 'evening';
-      }
-
-      return 'Hello and good ' + greet + '!';
-    },
-    workoutClose: function() {
-      // reset everything?
-      vm.workoutopen = false;
-      vm.workoutClear();
     },
     workoutClear: function() {
       vm.workoutSelectedData = [];
-      vm.timerSelected = '';
+      vm.isTimerSelected = '';
       vm.timerElapsed = null;
-      vm.currentWorkout = null;
-      vm.currentWorkoutImg = null;
-      vm.currentWorkoutNum = 0;
+      vm.currWorkout = null;
+      vm.currWorkoutImg = null;
+      vm.currWorkoutNum = 0;
     },
     workoutFetch: function(workout) {
-      // console.log(workout);
       fetch('assets/src/' + workout)
         .then((response) => {
           return response.json();
@@ -121,12 +86,13 @@ const vm = new Vue({
           for (let i = 0; i < data.workouts.length; i++) {
             vm.workoutSelectedData.push(data.workouts[i]);
           }
-          console.log('done: ', data.workouts);
+          vm.workoutLoad(0);
         });
     },
     workoutLoad: function(workoutnum) {
-      vm.currentWorkout = vm.workoutSelectedData[workoutnum].name;
-      vm.currentWorkoutImg = vm.workoutSelectedData[workoutnum].img;
+      vm.currWorkout = vm.workoutSelectedData[workoutnum].name;
+      vm.currWorkoutImg = vm.workoutSelectedData[workoutnum].img;
+      console.log('loading... ', vm.currWorkout);
 
       let limit = vm.workoutSelectedData.length - 1;
 
@@ -142,39 +108,64 @@ const vm = new Vue({
         vm.disablePrev = false;
       }
     },
-    workoutNext: function() {     
+    autoPlay: function() {
+
+    },
+    skipNext: function() {     
       let limit = vm.workoutSelectedData.length - 1;
       
-      if (vm.currentWorkoutNum <= limit) {
-        vm.currentWorkoutNum++;
-        vm.workoutLoad(vm.currentWorkoutNum);
+      if (vm.currWorkoutNum <= limit) {
+        vm.currWorkoutNum++;
+        vm.workoutLoad(vm.currWorkoutNum);
       } 
+
+      vm.timerReset();
     },
-    workoutPrev: function() {           
-      if (vm.currentWorkoutNum >= 1) {
-        vm.currentWorkoutNum--;
-        vm.workoutLoad(vm.currentWorkoutNum);
+    skipPrev: function() {
+      if (vm.currWorkoutNum >= 1) {
+        vm.currWorkoutNum--;
+        vm.workoutLoad(vm.currWorkoutNum);
       } 
+
+      vm.timerReset();
     },
     workoutSelected: function(workout) {
       vm.workoutFetch(workout);
-
-      setTimeout(() => {
-        vm.workoutLoad(0);
-        vm.workoutopen = true;
-      }, 350);
     }
   },
   mounted() {
-    // load workouts list
+    // load workouts list from file
     fetch('assets/src/workouts.json')
       .then((response) => {
         return response.json();
       })
       .then((data) => {
         vm.workoutList = data;
-        // console.log("kk", vm.workoutList);
-        vm.initList();
       });
+  },
+  computed: {
+    greet: function() {
+      const today = new Date().getHours();
+      let greet;
+
+      if (today < 12) {
+        greet = 'morning';
+      } else if (today < 18) {
+        greet = 'afternoon';
+      } else {
+        greet = 'evening';
+      }
+
+      return 'Hello and good ' + greet + '!';
+    },
+    timerDisplay: function() {
+      // display timeElapsed in 00:00 format
+      let ms = vm.timerElapsed;
+
+      // https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
+      let minutes = Math.floor( ms / 60000);
+      let seconds = ((ms % 60000) / 1000).toFixed(0);
+      return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
   }
 });
